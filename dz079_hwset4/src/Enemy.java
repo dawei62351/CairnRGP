452a77502acf3ea903f82e9204690470e5cfd98c
dawei62351 <66706679+dawei62351@users.noreply.github.com>
2023-03-06 08:59:16 -0500

    enemy

--- dz079_hwset4/src/Enemy.java
+++ dz079_hwset4/src/Enemy.java
@@ -1,17 +1,37 @@
+import java.util.Random;
 public class Enemy extends Character{
     private String name;
-    private int magicPower = 30;
-    Enemy(double hp, double a, boolean s, String n, int mp){
-        super(hp,a,s)
+
+    @java.lang.Override
+    public String getName() {
+        return name;
+    }
+
+    public float getMagicPower() {
+        return magicPower;
+    }
+
+    private float magicPower = 30;
+    Enemy(float h, float a, boolean al, String n, int mp){
+        super(h,ap,al)
         this.name = n;
         this.magicPower = mp;
     }
     Enemy(String n, int h, int ap, boolean ia){
         super(n,h,ap,ia)
     }
-    static void fight() {
-        Random rand = new Random();
-        int upperbound = 10;
-        int int_random = rand.nextInt(upperbound);
+    float fight() {
+        float damage;
+        int int_random = (int) (Math.random() * ((10) + 1));
+        float heroHp = Character.Hero.getHealth();
+        if (int_random < 3){
+            damage = getMagicPower();
+        } else {
+            damage = getAttackPower();
+        }
+        float final= heroHp - damage;
+        float final = heroHp - damage;
+        return final
+
     }
 }
