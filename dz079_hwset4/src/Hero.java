e6d7aa6ab5c342d4523d4398c30c312e6bb5ddb6
ElizavKim <elizav.kim@gmail.com>
2023-03-06 23:05:00 -0500

    Update Hero.java

--- dz079_hwset4/src/Hero.java
+++ dz079_hwset4/src/Hero.java
@@ -26,8 +26,7 @@ public class Hero extends Character {
 
 
     Hero(double h, double ap, boolean al) {
-        super(h, ap, al);
-        setName("Bob");
+        super("Bob", h, ap, al);
     }
 
     public boolean fight(Enemy e) {
