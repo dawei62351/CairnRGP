c5c2532f0670f4ceb373a8b9ba7038f4aad24e78
ElizavKim <elizav.kim@gmail.com>
2023-03-07 16:29:22 -0500

    added test files

---
+++ dz079_hwset4/test/HeroTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeroTest {
+
+    @Test
+    void fight() {
+    }
+
+    @Test
+    void levelUp() {
+        assertEquals();
+    }
+
+    @Test
+    void showInventory() {
+    }
+
+    @Test
+    void info() {
+    }
+}
