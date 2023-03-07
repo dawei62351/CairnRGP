51c5728ee713576ba502fd5d6de3d9c3c8d75f59
dawei62351 <dawei.z.62351@gmail.com>
2023-03-06 19:50:29 -0500

    adding fight and info function

--- dz079_hwset4/src/Boss.java
+++ dz079_hwset4/src/Boss.java
@@ -1,3 +1,34 @@
 public class Boss extends Enemy{
+    private String name;
+    int ultrasLeft;
+    Boss(double h, double ap, boolean ia){
+        super(h,ap,ia);
+        this.name = "Big Boss";
+        this.ultrasLeft = 3;
+    }
 
+    boolean fight(Hero h) {
+        double damage;
+        int int_random = (int) (Math.random() * ((10) + 1));
+        double heroHp = h.getHealth();
+        if (int_random == 0){
+            System.out.println("Attack missed: -0HP");
+            return false;
+        } else if (int_random<3 && this.ultrasLeft>0){
+            damage = this.getAttackPower()*3.5;
+            this.ultrasLeft -= 1;
+            System.out.println("Ultra attack: -" + damage +"HP");
+        } else {
+            damage = this.getAttackPower();
+            System.out.println("Normal attack: -" + damage +"HP");
+        }
+        return true;
+    }
+    String info(Boss b){
+        String message = "Boss name: " + this.name + "\n" +
+                "Boss health: " + this.getHealth() + "\n" +
+                "Boss attack power: " + this.getAttackPower() + "\n" +
+                "Boss ultra attack left: " + this.ultrasLeft;
+        return message;
+    }
 }
--- dz079_hwset4/src/Enemy.java
+++ dz079_hwset4/src/Enemy.java
@@ -10,23 +10,34 @@ public class Enemy extends Character{
         return magicPower;
     }
 
-    private double magicPower = 30;
-    Enemy(double h, double ap, boolean al, String n, int mp){
+    private double magicPower;
+    Enemy(double h, double ap, boolean al){
         super(h,ap,al);
-        this.name = n;
-        this.magicPower = mp;
+        this.name = "Enemy1";
+        this.magicPower = 30;
     }
-    float fight() {
-        float damage;
+    boolean fight(Hero h) {
+        double damage;
         int int_random = (int) (Math.random() * ((10) + 1));
-        float heroHp = Character.Hero.getHealth();
-        if (int_random < 3){
-            damage = getMagicPower();
+        double heroHp = h.getHealth();
+        if (int_random == 0){
+            System.out.println("Attack missed: -0HP");
+            return false;
+        } else if (int_random<3 && this.magicPower>5){
+            damage = this.getAttackPower()*2;
+            this.magicPower -= 5;
+            System.out.println("Magic attack: -" + damage +"HP");
         } else {
-            damage = getAttackPower();
+            damage = this.getAttackPower();
+            System.out.println("Normal attack: -" + damage +"HP");
         }
-        float finalHp= heroHp - damage;
-        return finalHp;
-
+        return true;
+    }
+    String info(Enemy e){
+        String message = "Enemy name: " + this.name + "\n" +
+                "Enemy health: " + this.getHealth() + "\n" +
+                "Enemy attack power: " + this.getAttackPower() + "\n" +
+                "Enemy magic power: " + this.magicPower;
+        return message;
     }
 }
