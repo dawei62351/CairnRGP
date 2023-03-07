c82e0c1f92a45820b003b590302259703123f5a2
ElizavKim <elizav.kim@gmail.com>
2023-03-07 17:18:30 -0500

    edits to hero and world tests

--- dz079_hwset4/test/WorldTest.java
+++ dz079_hwset4/test/WorldTest.java
@@ -1,19 +1,31 @@
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WorldTest {
+//    @Before
+//    void beforeTest() {
+//        var world = new World();
+//    }
 
     @Test
     void getGameName() {
+        var world = new World();
+        assertEquals("The Greatest RPG of All Time", world.getGameName());
     }
 
-    @Test
-    void setGameName() {
-    }
+//    @Test
+//    void setGameName() {
+//        var world = new World();
+//        setGameName()
+//        assertEquals("Another Game Name", world.setGameName());
+//    }
 
     @Test
     void getPublisher() {
+        var world = new World();
+        assertEquals("CIS Duets", world.getPublisher());
     }
 
     @Test
