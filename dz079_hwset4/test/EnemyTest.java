7f1a9e614cdda63f852f9f1cfcf1b06f65f168e8
dawei62351 <66706679+dawei62351@users.noreply.github.com>
2023-03-07 17:25:38 -0500

    update test

--- dz079_hwset4/test/EnemyTest.java
+++ dz079_hwset4/test/EnemyTest.java
@@ -3,13 +3,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EnemyTest {
-    
     Enemy e = new Enemy(100.0, 10.0, true);
     Hero h = new Hero(100.0, 10.0, true);
 
     @Test
     void getName() {
-        assertEquals("Enemy1", e.getName());
+        assertEquals("Ubda", e.getName());
     }
 
     @Test
@@ -23,7 +22,7 @@ class EnemyTest {
     }
     @Test
     void fightTestMagic() {
-        assertEquals(false, e.fightTest(h,2));
+        assertEquals(true, e.fightTest(h,2));
     }
     @Test
     void fightTestNormal1() {
@@ -34,11 +33,7 @@ class EnemyTest {
         assertEquals(true, e.fightTest(h,5));
     }
     @Test
-    void fightTestNormal3() {
-        assertEquals(false, e.fightTest(h,10));
-    }
-    @Test
     void info() {
-        assertEquals("Enemy name: Ubda\nEnemy health: 100.0HP\nEnemy attack power: 10.0\nEnemy magic power: 30", e.info());
+        assertEquals("Enemy name: Ubda\nEnemy health: 100.0HP\nEnemy attack power: 10.0\nEnemy magic power: 30.0", e.info());
     }
 }
