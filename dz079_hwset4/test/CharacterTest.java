ccba40585a8f5171d0ceb13be91e642e8337c80e
arnoldmark <mk772@cairn.edu>
2023-03-09 20:48:25 -0500

    updated character and item unit tests

--- dz079_hwset4/test/CharacterTest.java
+++ dz079_hwset4/test/CharacterTest.java
@@ -4,7 +4,11 @@ class CharacterTest {
 
 
     @Test
-    void info() {
+    void infoReturn() {
+        Character wizard = new Character("Max", 100.0, 10.0, true);
+        assertEquals("Max",100.0, 10.0, true,  wizard);
+    }
 
+    private void assertEquals(String max, double v, double v1, boolean b, Character wizard) {
     }
 }
